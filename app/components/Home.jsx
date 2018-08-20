bb9cfb15a0aa5811782d8397f0f34d61ec3efaab
chorong <chorong4641@naver.com>
2018-08-20 10:38:25 +0900

    home layout 분할

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -16,18 +16,9 @@ export default class Home extends Component {
   }
   render() {
     return (
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-        <div class="col-md-2 py-md-3 pl-md-5 bd-sidebar"> 
-          components
-        </div>
-        <main class="col-md-auto col-xl-8 py-md-3 pl-md-5 bd-content">
-          <FrogEditor pbUpdateHandler={this.forceUpdateHandler}/>
-        </main>
-        <div class="col-md-2 py-md-3 pl-md-5">
-          inspector
-        </div>
-      </div>
-    </div>);
+      <main class="col-md-auto col-xl-8 py-md-3 pl-md-5 bd-content">
+        <FrogEditor pbUpdateHandler={this.forceUpdateHandler}/>
+      </main>
+    );
   }
 }
--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,11 +1,27 @@
 // @flow
 import React, { Component } from 'react';
 import Home from '../components/Home';
+import SideComponent from './SideComponent';
+import SideInspector from './SideInspector';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class HomePage extends Component {
   render() {
-    return <Home />;
+    return (
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-md-2 py-md-3 pl-md-5 bd-sidebar">
+            <SideComponent />
+          </div>
+          <main class="col-md-auto col-xl-8 py-md-3 pl-md-5 bd-content">
+            <Home />
+          </main>
+          <div class="col-md-2 py-md-3 pl-md-5">
+            <SideInspector />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
