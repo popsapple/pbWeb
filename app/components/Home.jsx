73e58f02957fd1e500bcd7ec669b100251c9d294
chorong <chorong4641@naver.com>
2018-08-20 09:50:25 +0900

    가람씨 파일과 합침

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,24 +1,33 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import FrogEditor from './FrogEditor';
 import SideComponent from './SideComponent'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
-const Home = () => (
-  <div class="container-fluid">
-    <div class="row justify-content-center">
-      <div class="col-md-2 py-md-3 pl-md-5 bd-sidebar"> 
-        components
+export default class Home extends Component {
+   constructor(){
+    super();
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+  };
+  forceUpdateHandler(){
+  console.log("forceUpdateHandler");
+  	this.forceUpdate();
+  }
+  render() {
+    return (
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-2 py-md-3 pl-md-5 bd-sidebar"> 
+          components
+        </div>
+        <main class="col-md-auto col-xl-8 py-md-3 pl-md-5 bd-content">
+          <FrogEditor pbUpdateHandler={this.forceUpdateHandler}/>
+        </main>
+        <div class="col-md-2 py-md-3 pl-md-5">
+          inspector
+        </div>
       </div>
-      <main class="col-md-auto col-xl-8 py-md-3 pl-md-5 bd-content">
-        <FrogEditor/>
-      </main>
-      <div class="col-md-2 py-md-3 pl-md-5">
-        inspector
-      </div>
-    </div>
-  </div>
-)
-
-export default Home;
+    </div>);
+  }
+}
