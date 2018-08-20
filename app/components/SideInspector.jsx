4375bc1a975ad0394c5c2801ebff0eae0fe200ad
garam.hyeon <garam.hyeon@clbee.com>
2018-08-20 17:12:45 +0900

    인스펙터내용추가

--- app/components/SideInspector.jsx
+++ app/components/SideInspector.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import CompomentInfo from './inspector/CompomentInfo';
+import WorkingTree from './inspector/WorkingTree';
 
 export default class SideInspector extends React.Component{
     render(){
         return(
-            <div>inspector</div>
+            <div className="component_list">
+            	<button>Info</button>
+            	<button>Tree</button>
+            	<CompomentInfo />
+            	<WorkingTree />
+            </div>
         )
     }
 }
