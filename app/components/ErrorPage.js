c6657915f992c98de1b5cb3510de9c220385a2d6
chorong <chorong4641@naver.com>
2018-08-27 13:45:08 +0900

    가람씨 코드로 수정

---
+++ app/components/ErrorPage.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+export default class ErrorPage extends Component {
+    constructor(){
+      super();
+    };
+  
+    render() {
+      return (
+          <div>
+              404 Error Page
+          </div>
+      );
+    }
+  }
